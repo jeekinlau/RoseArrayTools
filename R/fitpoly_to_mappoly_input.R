beae85c910a2b89da7662d0f91083274cebd2fb9
Jeekin Lau <jeekinlau@gmail.com>
2022-01-31 22:04:47 -0600

    Update fitpoly_to_mappoly_input.R

--- R/fitpoly_to_mappoly_input.R
+++ R/fitpoly_to_mappoly_input.R
@@ -1,10 +1,31 @@
 input_file = "96_SWxBE_scores_compared_calls.csv"
 
-
-
+input_file_name = gsub(".csv","",input_file)
 file = read.csv(input_file)
+genomic_pos = read.table("https://raw.githubusercontent.com/jeekinlau/RoseArrayTools/add_fitpoly_to_mappoly_format_funtionality/docs/for_add_genomic_positions_saintoyant.txt", header = T, sep="\t")
+
+temp_names = names(file)
+temp_names[1] = "Marker"
+
+colnames(file) = temp_names
+
 p1= "Brite_Eyes_8"
 p2= "Stormy_Weather_3"
 
-p1_geno = file[,which(colnames(file)==p1)]
-p2_geno = file[,which(colnames(file)==p2)]
+merged = merge(file,genomic_pos, by="Marker", all=T)
+
+p1_geno = merged[,which(colnames(merged)==p1)]
+p2_geno = merged[,which(colnames(merged)==p2)]
+
+merged_progeny = merged[,-which(colnames(merged)%in%c(p1,p2,"Marker","LG","Position"))]
+
+final = cbind(merged$Marker, p1_geno, p2_geno, merged$LG, merged$Position, merged_progeny)
+final_colnames <- colnames(final)
+final_colnames[1:5] = c("Marker", "P1", "P2", "LG", "Position")
+colnames(final) = final_colnames
+
+final = final[order(final$LG, final$Position),]
+final = final[-which(is.na(final$P1)),]
+final = final[-which(is.na(final$P2)),]
+
+write.csv(final,paste0(input_file_name,"_mappoly_ready.csv"),row.names = F)
