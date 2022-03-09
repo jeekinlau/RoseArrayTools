898e1df79ac966fa340c26c148d4e706df4ee666
Jeekin Lau <jeekinlau@gmail.com>
2022-03-09 09:46:00 -0600

    Update fitpoly_to_polyOrigin_input.R

--- R/fitpoly_to_polyOrigin_input.R
+++ R/fitpoly_to_polyOrigin_input.R
@@ -1,27 +1,28 @@
 fitPoly_to_polyOrigin_input<-function(input_file){
-  
-  
+
+
   input_file_name = gsub(".csv","",input_file)
-  file = read.csv(input_file)
+  file = read.csv(input_file, header = F)
   genomic_pos = read.table("https://raw.githubusercontent.com/jeekinlau/RoseArrayTools/master/docs/for_add_genomic_positions_saintoyant.txt", header = T, sep="\t")
-  
-  temp_names = names(file)
+
+  temp_names = file[1,]
   temp_names[1] = "Marker"
-  
+
+  file = file[-1,]
   colnames(file) = temp_names
-  
-  
+
+
   merged = merge(file,genomic_pos, by="Marker", all=T)
 
   merged_genos = merged[,-which(colnames(merged)%in%c("Marker","LG","Position"))]
-  
+
   final = cbind(merged$Marker, merged$LG, merged$Position, merged_genos)
   final_colnames <- colnames(final)
   final_colnames[1:3] = c("marker",  "chromosome", "position")
   colnames(final) = final_colnames
-  
+
   final = final[order(final$chromosome, final$position),]
-  
+
   write.csv(final,paste0(input_file_name,"_polyOrigin_ready.csv"),row.names = F)
   print("done")
 }
