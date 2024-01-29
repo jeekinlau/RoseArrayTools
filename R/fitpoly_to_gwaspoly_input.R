2542b6f44152c376e2143f3b62ef0d9c245a8920
Jeekin Lau <jeekinlau@gmail.com>
2024-01-29 16:23:51 -0600

    update_perfect_GWASpoly_positions

--- R/fitpoly_to_gwaspoly_input.R
+++ R/fitpoly_to_gwaspoly_input.R
@@ -7,7 +7,7 @@
 #' @return a .csv file that has the added columns of genome position and LG of markers. Will contain markers with no known location at bottom of file.
 #'
 #' @export fitPoly_to_GWASpoly_input
-#'
+#' @export fitpoly_to_GWASpoly_input_perfect
 #'
 #'
 #'
@@ -21,6 +21,30 @@ fitPoly_to_GWASpoly_input <- function(input_file){
   genomic_pos = read.table("https://raw.githubusercontent.com/jeekinlau/RoseArrayTools/master/data/for_add_genomic_positions_saintoyant2.txt", header = T, sep="\t")
 
   temp_names = names(file)
+  temp_names[1] = "Marker"
+  colnames(file) = temp_names
+
+  merged = merge(file,genomic_pos, by="Marker", all=T)
+
+  merged_genotypes = merged[,-which(colnames(merged)%in%c("Marker","LG","Position"))]
+
+  final = cbind(merged$Marker,  merged$LG, merged$Position, merged_genotypes)
+  final_colnames <- colnames(final)
+  final_colnames[1:3] = c("Marker", "Chrom", "Position")
+  colnames(final) = final_colnames
+  final = final[order(final$Chrom, final$Position),]
+  write.csv(final,paste0(input_file_name,"_GWAS_ready.csv"),row.names = F)
+  print("done")
+}
+
+
+
+fitPoly_to_GWASpoly_input_perfect <- function(input_file){
+  input_file_name = gsub(".csv","",input_file)
+  file = read.csv(input_file)
+  genomic_pos = read.csv("https://raw.githubusercontent.com/jeekinlau/RoseArrayTools/master/data/perfect_fits_saintoyant.csv", header = T)
+
+  temp_names = colnames(file)
   temp_names[1] = "Marker"
   colnames(file) = temp_names
 
